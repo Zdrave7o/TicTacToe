a3d6e95c7d5a6f14f9de45826b39ba9e301bc0bf
Zdravko Georgiev <zzgzdravko@gmail.com>
2025-09-16 12:07:35 +0300

    backing up

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const player1Slots = [];
 const player2Slots = [];
 const slots = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
+const player1 = "Player1";
+const player2 = "Player2"
+
 let currentPlayer = "player1";
 
 const documentSlots = document.querySelectorAll(".slot");
