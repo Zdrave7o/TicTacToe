cc72ae0a85fa9bad41aebde2fc9f465e3a11efe2
Zdravko Georgiev <zzgzdravko@gmail.com>
2025-09-16 19:43:03 +0300

    saving work

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const player2Slots = [];
 const slots = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 const player1 = "Player1";
-const player2 = "Player2";
+const player2 = "computer";
 let symbol = "X";
 
 let currentPlayer = player1;
@@ -12,9 +12,9 @@ const documentSlots = document.querySelectorAll(".slot");
 
 window.addEventListener("DOMContentLoaded", activateSlotsEventListeners);
 
-function selectSlot(player, currentArr, slotValue, slot){
-    if(slots.includes(slotValue)){
-        player === player1? symbol="X":symbol ="O";
+function selectSlot(player, currentArr, slotValue, slot) {
+    if (slots.includes(slotValue)) {
+        player === player1 ? symbol = "X" : symbol = "O";
 
         let removableIndex = slots.indexOf(slotValue);
         slots.splice(removableIndex, 1);
@@ -23,14 +23,14 @@ function selectSlot(player, currentArr, slotValue, slot){
 
         currentArr.push(slotValue);
 
-        if (currentArr.includes("1") && currentArr.includes("2") && currentArr.includes("3") || 
-        currentArr.includes("4") && currentArr.includes("5") && currentArr.includes("6") || 
-        currentArr.includes("7") && currentArr.includes("8") && currentArr.includes("9") || 
-        currentArr.includes("1") && currentArr.includes("4") && currentArr.includes("7") ||
-        currentArr.includes("2") && currentArr.includes("5") && currentArr.includes("8") ||
-        currentArr.includes("3") && currentArr.includes("6") && currentArr.includes("9") ||
-        currentArr.includes("1") && currentArr.includes("5") && currentArr.includes("9") ||
-        currentArr.includes("3") && currentArr.includes("5") && currentArr.includes("7")){
+        if (currentArr.includes("1") && currentArr.includes("2") && currentArr.includes("3") ||
+            currentArr.includes("4") && currentArr.includes("5") && currentArr.includes("6") ||
+            currentArr.includes("7") && currentArr.includes("8") && currentArr.includes("9") ||
+            currentArr.includes("1") && currentArr.includes("4") && currentArr.includes("7") ||
+            currentArr.includes("2") && currentArr.includes("5") && currentArr.includes("8") ||
+            currentArr.includes("3") && currentArr.includes("6") && currentArr.includes("9") ||
+            currentArr.includes("1") && currentArr.includes("5") && currentArr.includes("9") ||
+            currentArr.includes("3") && currentArr.includes("5") && currentArr.includes("7")) {
             let winInterval = setInterval(() => {
                 window.alert(`${currentPlayer} wins!`);
                 clearInterval(winInterval);
@@ -38,7 +38,7 @@ function selectSlot(player, currentArr, slotValue, slot){
             }, 50);
 
             return;
-        } else if(slots.length < 1){
+        } else if (slots.length < 1) {
             let winInterval = setInterval(() => {
                 window.alert(`Draw!`);
                 clearInterval(winInterval);
@@ -46,23 +46,43 @@ function selectSlot(player, currentArr, slotValue, slot){
             }, 50);
         }
 
-        currentPlayer = player === player1? player2:player1;
-    } else{
+        currentPlayer = player === player1 ? player2 : player1;
+
+        if (player2 === "computer" && player === player1) {
+            currentPlayer = player2;
+            let choice = slots[Math.floor(Math.random() * slots.length)];
+            console.log(`choice ${choice}`);
+
+            currentArr = player2Slots;
+
+            setTimeout(() => {
+                // code to run after 0.5 seconds
+                selectSlot(currentPlayer, currentArr, choice, documentSlots[Number(choice) - 1]);
+                activateSlotsEventListeners;
+            }, 50);
+
+        }
+
+    } else {
         console.log(`${slotValue} is marked`)
     }
-}   
 
-function activateSlotsEventListeners(){
+}
+
+function activateSlotsEventListeners() {
     documentSlots.forEach(slot => {
-    slot.addEventListener("click",() =>{
+        slot.addEventListener("click", () => {
+            let slotValue = slot.getAttribute("index");
+            console.log(`clicked Value Element -> ${slotValue}`)
 
-        let slotValue = slot.getAttribute("index"); 
-        console.log(`clicked Value Element -> ${slotValue}`)
-        
-        let currentArr = currentPlayer === player1? player1Slots:player2Slots
+            let currentArr = currentPlayer === player1 ? player1Slots : player2Slots
 
-        selectSlot(currentPlayer, currentArr, slotValue, slot);
-    } )
+            selectSlot(currentPlayer, currentArr, slotValue, slot);
+        })
+
+    })
+}
+
+function getSlotValue(){
     
-})
 }
