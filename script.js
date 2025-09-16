e7101da3c1da8823224a608275b40d39a0c095c1
Zdravko Georgiev <zzgzdravko@gmail.com>
2025-09-16 12:17:46 +0300

    simplified logic

--- script.js
+++ script.js
@@ -2,72 +2,50 @@ const player1Slots = [];
 const player2Slots = [];
 const slots = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-let currentPlayer = "player1";
+const player1 = "Player1";
+const player2 = "Player2";
+let symbol = "X";
+
+let currentPlayer = player1;
 
 const documentSlots = document.querySelectorAll(".slot");
 
 window.addEventListener("DOMContentLoaded", activateSlotsEventListeners);
 
-function selectSlot(player, slotValue, slot){
+function selectSlot(player, currentArr, slotValue, slot){
     if(slots.includes(slotValue)){
+        player === player1? symbol="X":symbol ="O";
+
+
         let removableIndex = slots.indexOf(slotValue);
         slots.splice(removableIndex, 1);
         console.log(`${currentPlayer} chose ${slotValue}`);
-        const sortedArr = [];
-        if(player === "player1"){
-            slot.innerHTML = "<h1>X</h1>";
+        slot.innerHTML = `<h1>${symbol}</h1>`;
 
-            player1Slots.push(slotValue);
+        currentArr.push(slotValue);
 
-            player1Slots.forEach(item => {
-                item = Number();
-            })
-            player1Slots.sort((x, y) => x - y);
+        currentArr.forEach(item => {
+            item = Number();
+        })
+        currentArr.sort((x, y) => x - y);
 
-            let str = player1Slots.join("");
-            
-            if(str.includes("123") || str.includes("456") || str.includes("789")){
-                window.alert(`${currentPlayer} wins!`);
-                return;
-            } else if(str.includes("1") && str.includes("4") && str.includes("7") || 
-              (str.includes("2") && str.includes("5") && str.includes("8")) ||
-              (str.includes("3") && str.includes("6") && str.includes("9"))){
-                window.alert(`${currentPlayer} wins!`);
-                return;
-            } else if(str.includes("1") && str.includes("5") && str.includes("9") ||
-             (str.includes("3") && str.includes("5") && str.includes("7"))){
-                window.alert(`${currentPlayer} wins!`);
-                return;
-            }
+        let str = currentArr.join("");
 
-            currentPlayer = "player2";
-        } else{
-            slot.innerHTML = "<h1>O</h1>";
-
-            player2Slots.push(slotValue);
-
-            player2Slots.forEach(item => {
-                item = Number();
-            })
-            player2Slots.sort((x, y) => x - y);
-
-            let str = player2Slots.join("");
-            
-            if(str.includes("123") || str.includes("456") || str.includes("789")){
-                window.alert(`${currentPlayer} wins!`);
-                return;
-            } else if(str.includes("1") && str.includes("4") && str.includes("7") || 
-              (str.includes("2") && str.includes("5") && str.includes("8")) ||
-              (str.includes("3") && str.includes("6") && str.includes("9"))){
-                window.alert(`${currentPlayer} wins!`);
-                return;
-            } else if(str.includes("1") && str.includes("5") && str.includes("9") ||
-             (str.includes("3") && str.includes("5") && str.includes("7"))){
-                window.alert(`${currentPlayer} wins!`);
-                return;
-            }
-            currentPlayer = "player1";
+        if (str.includes("123") || str.includes("456") || str.includes("789")) {
+            window.alert(`${currentPlayer} wins!`);
+            return;
+        } else if (str.includes("1") && str.includes("4") && str.includes("7") ||
+            (str.includes("2") && str.includes("5") && str.includes("8")) ||
+            (str.includes("3") && str.includes("6") && str.includes("9"))) {
+            window.alert(`${currentPlayer} wins!`);
+            return;
+        } else if (str.includes("1") && str.includes("5") && str.includes("9") ||
+            (str.includes("3") && str.includes("5") && str.includes("7"))) {
+            window.alert(`${currentPlayer} wins!`);
+            return;
         }
+
+        currentPlayer = player === player1? player2:player1;
     } else{
         console.log(`${slotValue} is marked`)
     }
@@ -79,7 +57,10 @@ function activateSlotsEventListeners(){
 
         let slotValue = slot.getAttribute("index"); 
         console.log(`clicked Value Element -> ${slotValue}`)
-        selectSlot(currentPlayer, slotValue, slot);
+        
+        let currentArr = currentPlayer === player1? player1Slots:player2Slots
+
+        selectSlot(currentPlayer, currentArr, slotValue, slot);
     } )
     
 })
