7ffcb25e566478960a0d1e75a5b609b7a1c87716
Zdravko Georgiev <zzgzdravko@gmail.com>
2025-09-23 19:30:44 +0300

    updating to most recent changes

--- script.js
+++ script.js
@@ -26,18 +26,18 @@ opponentBtn.onclick = changeOpponent;
 
 window.addEventListener("DOMContentLoaded", activateSlotsEventListeners);
 
-function selectSlot(player, currentArr, slotValue, slot) {
-    if (slots.includes(slotValue)) {
+function selectSlot(player, currentPlayerArr, slotIndex, slot) {
+    if (slots.includes(slotIndex)) {
         player === player1 ? symbol = "X" : symbol = "O";
 
-        let removableIndex = slots.indexOf(slotValue);
+        let removableIndex = slots.indexOf(slotIndex);
         slots.splice(removableIndex, 1);
-        console.log(`${currentPlayer} chose ${slotValue}`);
+        console.log(`${currentPlayer} chose ${slotIndex}`);
         slot.innerHTML = `<h1>${symbol}</h1>`;
 
-        currentArr.push(slotValue);
+        currentPlayerArr.push(slotIndex);
 
-        if (hasWinningCombo(currentArr)) {
+        if (hasWinningCombo(currentPlayerArr)) {
             let winInterval = setInterval(() => {
                 window.alert(`${currentPlayer} wins!`);
                 clearInterval(winInterval);
@@ -61,7 +61,7 @@ function selectSlot(player, currentArr, slotValue, slot) {
 
             console.log(`choice ${choice}`);
 
-            currentArr = player2Slots;
+            currentPlayerArr = player2Slots;
             documentSlots.forEach(slot => {
                 slot.onclick = () => {
                     window.alert("please wait!");
@@ -71,14 +71,14 @@ function selectSlot(player, currentArr, slotValue, slot) {
 
             setTimeout(() => {
                 // code to run after 0.5 seconds
-                selectSlot(currentPlayer, currentArr, choice, documentSlots[Number(choice) - 1]);
+                selectSlot(currentPlayer, currentPlayerArr, choice, documentSlots[Number(choice) - 1]);
                 activateSlotsEventListeners();
             }, 500);
 
         }
 
     } else {
-        console.log(`${slotValue} is marked`)
+        console.log(`${slotIndex} is marked`)
     }
 
 }
@@ -86,12 +86,12 @@ function selectSlot(player, currentArr, slotValue, slot) {
 function activateSlotsEventListeners() {
     documentSlots.forEach(slot => {
         slot.onclick = () => {
-            let slotValue = slot.getAttribute("index");
-            console.log(`clicked Value Element -> ${slotValue}`)
+            let slotIndex = slot.getAttribute("index");
+            console.log(`clicked Value Element -> ${slotIndex}`)
 
-            let currentArr = currentPlayer === player1 ? player1Slots : player2Slots
+            let currentPlayerArr = currentPlayer === player1 ? player1Slots : player2Slots
 
-            selectSlot(currentPlayer, currentArr, slotValue, slot);
+            selectSlot(currentPlayer, currentPlayerArr, slotIndex, slot);
         }
 
     })
